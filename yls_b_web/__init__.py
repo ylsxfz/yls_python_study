44eb6fa6ca4659c46e0fc087728656d29bffc4ae
yls <2689623200@qq.com>
2020-07-24 10:50:17 +0800

    文件分包修改

---
+++ yls_b_web/__init__.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 _*_
+"""
+ @Time : 2020/7/24 10:02 
+ @Author : yls 
+ @Version：V 0.1
+ @File : __init__.py.py
+ @desc : web相关的模块学习
+ """
+
+if __name__ == '__main__':
+    pass
--- yls_c_artificial_intelligence/__init__.py
+++ yls_c_artificial_intelligence/__init__.py
@@ -1,10 +1,10 @@
 # _*_ coding: utf-8 _*_
 """
- @Time : 2020/7/24 10:02 
+ @Time : 2020/7/24 10:47 
  @Author : yls 
  @Version：V 0.1
  @File : __init__.py.py
- @desc :
+ @desc : 8 个数据分析、机器学习和深度学习包和框架和入门案例总结
  """
 
 if __name__ == '__main__':
