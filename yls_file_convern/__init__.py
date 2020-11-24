de10efa8f4ade6f74906495dfeec5af5e2bd53d6
yls <2689623200@qq.com>
2020-11-24 16:10:34 +0800

    文件读取

---
+++ yls_file_convern/__init__.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 _*_
+"""
+ #  @Time : 2020/11/11 16:05 
+ #  @Author : yls 
+ #  @Version：V 0.1
+ #  @File : __init__.py.py
+ #  @desc :
+ """
+
+if __name__ == '__main__':
+    pass
